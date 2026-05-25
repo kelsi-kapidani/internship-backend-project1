9b84e73a46454f166cc03dff1a855488bdc6b7e6
unknown <kapidanikelsi13@gmail.com>
2026-05-25 14:10:01 +0200

    resolved an n+1 query problem instance

--- library/src/main/java/com/gisdev/library/repository/LibraryOrderRepository.java
+++ library/src/main/java/com/gisdev/library/repository/LibraryOrderRepository.java
@@ -3,6 +3,7 @@ package com.gisdev.library.repository;
 import com.gisdev.library.constants.enums.Status;
 import com.gisdev.library.entity.LibraryOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +11,13 @@ import java.util.List;
 @Repository
 public interface LibraryOrderRepository extends JpaRepository<LibraryOrder, Long> {
 
+    @Query("""
+    SELECT DISTINCT o
+    FROM LibraryOrder o
+    LEFT JOIN FETCH o.user
+    LEFT JOIN FETCH o.books bo
+    LEFT JOIN FETCH bo.book
+    WHERE o.status = :status
+    """)
     List<LibraryOrder> findAllByStatus(Status status);
 }
